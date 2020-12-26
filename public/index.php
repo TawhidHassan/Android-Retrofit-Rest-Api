a1f426f7d8c5e3100af78646b69492e147776bf0
sifat007 <sifat27.ht@gmail.com>
2020-12-26 17:47:52 +0600

    connection database

--- public/index.php
+++ public/index.php
@@ -1,15 +1,20 @@
 <?php
+use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
-
+require '../includes/DbConnect.php';
 $app = AppFactory::create();
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+	$response->getBody()->write("Hello, $name");
+	
+	$db=new DbConnect;
+	if($db->connect()!=null){
+		echo 'connection successfull';
+	}
     return $response;
 });
 
